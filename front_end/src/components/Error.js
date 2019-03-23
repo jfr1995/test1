9ddbe495ca2a83ed8a0f388a1c431a7c21cc200c
Juan Ramirez <juanramirez1776@gmail.com>
2019-03-23 01:05:12 -0700

    first commit

---
+++ front_end/src/components/Error.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <h1>There was an issue rendering the content</h1>
+    </div>
+  );
+};
+
+export default Error;
